b7950bf252097d0835d9a38ef2010cd2b60e78d6
ZoeMeredith <74957235+ZoeMeredith@users.noreply.github.com>
2021-01-11 13:47:28 +0100

    extracted ula-server connector

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,26 +2,23 @@
 /* eslint-disable no-unused-expressions */
 
 import { expect } from 'chai'
+import sinon from 'sinon'
+
 import UlaServerConnector from '../src/index'
 
-
 describe('The ULA Server Connector', () => {
-    it('should work in integration', async () => {
-        const conn = new UlaServerConnector();
-        conn.configure(process.env.ULA_ENDPOINT, process.env.ULA_BASIC_AUTH);
+  it('should work in integration', async () => {
+    const conn = new UlaServerConnector()
+    conn.configure('https://localhost:8080', '')
+    const resultLink = await conn.claim('123123123', '', { data: 'important' })
+    expect(resultLink).to.be.a('string')
+    expect(resultLink.startsWith('link:discipl:ula-server')).to.equal(true)
 
+    const claim = conn.get(resultLink)
 
-        const resultLink = await conn.claim("123123123", "", {"data": "important"})
-
-        expect(resultLink).to.be.a('string')
-        expect(resultLink.startsWith("link:discipl:ula-server")).to.equal(true)
-
-        const claim = await conn.get(resultLink);
-
-        expect(claim.qrcode).to.be.a('string');
-        expect(claim.sessionId).to.be.a('string');
-        expect(claim.transactionId).to.be.a('string');
-        
-    })
+    expect(claim.inviteURL).to.be.a('string')
+    expect(claim.operationType).to.be.a('string')
+    expect(claim.documentName).to.be.a('string')
+  })
 }
 )
